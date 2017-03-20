3c8bcf397c7d4697257e9a07109920a185723776
Marc Magon <ged_marc@hotmail.com>
2017-03-20 18:25:44 +0200

    v0.9.1 description messages

--- src/za/co/mmagon/jwebswing/plugins/dynamicsourcecode/DynamicSourceCodeFeature.java
+++ src/za/co/mmagon/jwebswing/plugins/dynamicsourcecode/DynamicSourceCodeFeature.java
@@ -1,4 +1,4 @@
-/* 
+/*
  * Copyright (C) 2017 Marc Magon
  *
  * This program is free software: you can redistribute it and/or modify
@@ -20,6 +20,7 @@ import za.co.mmagon.jwebswing.Component;
 import za.co.mmagon.jwebswing.Feature;
 import za.co.mmagon.jwebswing.base.html.interfaces.GlobalFeatures;
 import za.co.mmagon.jwebswing.plugins.google.sourceprettify.SourceCodePrettifyThemes;
+import za.co.mmagon.jwebswing.plugins.jquery.JQueryPageConfigurator;
 
 /**
  * Adds the dynamic source code JavaScript through
@@ -68,7 +69,7 @@ public class DynamicSourceCodeFeature extends Feature<DynamicSourceCodeOptions, 
     {
         if (!isConfigured())
         {
-            getComponent().getPage().getOptions().setjQueryEnabled(true);
+            JQueryPageConfigurator.setRequired((Component) getComponent(), true);
         }
         super.preConfigure();
     }
